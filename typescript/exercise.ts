83ba4a6ac05c468bed20fab58fd7647be9ea391e
Moh Sopyan <mohsopyan29@gmail.com>
2024-09-13 20:54:59 +0900

    modified exercise.ts

--- typescript/exercise.ts
+++ typescript/exercise.ts
@@ -1,36 +1,44 @@
-// Soal 1 "Luas Persegi Panjang"
-// Rumus luas persegi panjang (length * width)
+// Soal 1 "Mencari Luas Persegi Panjang"
 
-let _length: number = 5;
-let _width: number = 3;
-const _broad: number = _length * _width;
-console.log(`Luas persegi panjang adalah ${_broad}`);
+let _length: number = 3;
+let _width: number = 5;
+
+const broad: number = _length *  _width;
+console.log(`Luas persegi panjang adalah ${broad}`);
 
 // Soal 2 "Keliling Persegi Panjang"
-// Rumus keliling persegi panjang (2 * (length + width))
-const _around: number = 2 * (_length + _width);
-console.log(`Keliling persegi panjang adalah ${_around}`);
+
+let length2: number = 3;
+let width2: number = 5;
+const around: number = 2 * (length2 + width2);
+console.log(`Keliling persegi panjang adalah ${around}`);
 
 // Soal 3
+
 // a "Diameter Lingkaran" Rumus(2 * radius)
-let _radius: number = 5;
-const _diameter: number = 2 * _radius;
-console.log(`Diameter lingkaran adalah ${_diameter}`);
+let radius: number = 5;
+const diameter: number = 2 * radius;
+console.log(`Diameter lingkaran adalah ${diameter}`);
 
 // b "Keliling Lingkaran" Rumus(2PI_radius)
-const _circumference: number = 2 * Math.PI * _radius;
-console.log(`Keliling lingkaran adalah ${_circumference.toFixed(4)}`);
+const pi: number = Math.PI;
+const circumference: number = pi * diameter;
+console.log(`Keliling lingkaran adalah ${circumference.toFixed(4)}`);
 
 // c "Luas lingkaran" Rumus(PI * radius(2))
-const _area: number = Math.PI * Math.pow(_radius, 2);
-console.log(`Luas lingkaran adalah ${_area.toFixed(3)}`);
+// const _area: number = pi * Math.pow(_radius, 2);
+const area: number = pi * (radius * radius);
+console.log(area)
+console.log(`Luas lingkaran adalah ${area.toFixed(3)}`);
 
 // Soal 4 "Sudut ketiga dari Segitiga dengan x=80, y=65"
-// Rumus ( 180-(x + y) )
-let x:number = 80;
-let y:number = 65;
-const z:number = 180 - (x + y);
-console.log(`Sudut ketiganya adalah ${z}`);
+
+let corner1: number = 80;
+let corner2: number = 65;
+let total: number = 180;
+
+const corner3: number = total - (corner1 + corner2)
+console.log(`Sudut ketiganya adalah ${corner3}`);
 
 // Soal 5 "Nyatakan Jumlah hari dengan tahun, bulan dan hari"
 // 1 tahun = 365 hari, 1 bulan = 30 hari
@@ -60,17 +68,21 @@ console.log(`${_jumlahHari} hari : ${_tahun} tahun, ${_bulan} bulan ${_hari} har
 // Soal 6
 // Menghitung perbedaan hari antara dua tanggal
 
-let date1: string = "2022-01-20";
-let date2: string = "2022-01-22";
+// const startDate: any = new Date("2022-01-20");
+// const endDate: any = new Date("2022-01-22");
 
-const startDate: any = new Date(date1);
-const endDate: any = new Date(date2);
+// startDate.setHours(0, 0, 0, 0);
+// endDate.setHours(0, 0, 0, 0);
 
-startDate.setHours(0, 0, 0, 0);
-endDate.setHours(0, 0, 0, 0);
+// const oneDay: number = 24 * 60 * 60 * 1000;
+// const diff: number = Math.abs(startDate - endDate);
+// const day: number = Math.round(diff / oneDay);
 
-const oneDay: number = 24 * 60 * 60 * 1000;
-const diff: number = Math.abs(startDate - endDate);
-const day: number = Math.round(diff / oneDay);
+// console.log(`Perbedaan antara tanggal adalah ${day} hari`);
 
-console.log(`Perbedaan antara tanggal adalah ${day} hari`);
+
+
+const startDate: Date = new Date("2022-01-20");
+const endDate: Date = new Date("2022-01-22");
+const diff = (endDate.getTime() - startDate.getTime()) / (24 * 3600 * 1000);
+console.log(diff);
